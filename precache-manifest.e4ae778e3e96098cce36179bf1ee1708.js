1633c892c850ce396c39e7c61933eeb848604cdd
Avijit Nanda <avijitnanda14@gmail.com>
2020-05-12 23:12:53 +0530

    Updates

---
+++ precache-manifest.e4ae778e3e96098cce36179bf1ee1708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885ae93edd2b71bc5fd75cf8b2498db7",
+    "url": "/module2-project/index.html"
+  },
+  {
+    "revision": "b0bfdb3a607b85b3f4c5",
+    "url": "/module2-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a22224e7a6f2931de03",
+    "url": "/module2-project/static/js/2.e4910db4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/module2-project/static/js/2.e4910db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0bfdb3a607b85b3f4c5",
+    "url": "/module2-project/static/js/main.57d5f881.chunk.js"
+  },
+  {
+    "revision": "93927508875908d5b607",
+    "url": "/module2-project/static/js/runtime-main.189834d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/module2-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
